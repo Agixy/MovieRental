4d29a25c9e335c0440e511e7e214187efd6e13f6
Agnieszka <you@example.com>
2018-08-05 21:54:12 +0200

    Finish Edit Customer Function

--- Vidly/ViewModels/CustomerFormViewModel.cs
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -6,7 +6,7 @@ using Vidly.Models;
 
 namespace Vidly.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }      // view i domain model w dużych app sa inne ale tutaj możemy tego nie rozdzielac bo mała app
